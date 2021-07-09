bb1b18888d0252f0e37c8ea9efe2c0b3d0fa39f0
mrunali2007 <79053806+mrunali2007@users.noreply.github.com>
2021-07-09 16:52:45 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,30 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+      this.image = loadImage("block.png");
+      this.Visiblity = 255;
+    }
+    display(){
+      var pos= this.body.position;
+      imageMode(CENTER);
+   //   rect(pos.x,pos.y,this.width, this.height);
+    
+      if(this.body.speed > 3){
+        image(this.image,this.body.position.x,this.body.position.y,30,40);
+      }else{
+        World.remove(world,this.body);
+        push()
+        this.Visiblity = this.Visiblity-5;
+        tint(255,this.Visiblity);
+        image(this.image,this.body.position.x,this.body.position.y,30,40);
+        pop()
+    }
+}
+}
